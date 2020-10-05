52d58a4b560c7a55d403b2adeed5bba2cccf4b1d
raihan862 <raihankabir862@gamil.com>
2020-10-05 23:19:47 +0600

    update

--- index.js
+++ index.js
@@ -85,6 +85,6 @@ app.delete('/delete-activity/:id', (req, res)=> {
 });
 
  
-app.listen(5000, (req, res) => {
+app.listen(process.env.PORT || 5000, (req, res) => {
     console.log("app is listening to 5000");
 })
